085e6209d73cde7501bd3a0c1621f6f18a711e30
selfmotivater14@gmil.com <selfmotivater14@gmail.com>
2020-09-20 07:00:24 +0545

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 function Navbar() {
     return (
-        <>
+        <div className='App'>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo">
+                        TRVL <i className="fab fa-typo3"></i>
 
-                    </link>
+                    </Link>
                 </div>
 
             </nav>
-        </>
+        </div>
     )
 }
 
